30b7846028bee0d688b7b3f72bfb474c7e964bae
Daniel Polek-Pawlak <polekpaw@student.agh.edu.pl>
2018-02-03 11:05:57 +0100

    All Tests Green

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -12,11 +12,21 @@ public abstract class Product {
 	private final BigDecimal priceWithTax;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
+		if ( name == null || name.isEmpty() ) {
+			throw new IllegalArgumentException("Name is empty or null");
+		}
+		if ( price == null || price.signum() == -1) {
+			throw new IllegalArgumentException("Price is negative or null");
+			 
+		}
+		
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
-		priceWithTax = (price.multiply(tax).add(price));
-	}
+		this.priceWithTax = (price.multiply(taxPercent).add(price));
+	
+		}
+	
 
 	public String getName() {
 		return name;
